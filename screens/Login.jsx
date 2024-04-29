e28e3fceadf2b054d7e19f0e0aab1213386dcd0c
mydeenmk <a.mydeenmohammed@gmail.com>
2024-04-29 18:32:25 +0530

    main

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,130 +1,4 @@
-// import React, { useState } from 'react';
-// import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-// import Ionicons from '@expo/vector-icons/Ionicons';
-// import Evilicons from '@expo/vector-icons/EvilIcons';
-// import FontAwsome from '@expo/vector-icons/FontAwesome';
 
-// const Login = () => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-  
-
-//   const navigation = useNavigation(); // Initialize navigation hook
-   
-//   const LandingPage = () => {
-//     navigation.navigate('Landingpage');
-//   };
-
-
-//   const HomeScreen = () => {
-//     // Basic authentication logic (replace with your actual authentication process)
-//     if (username === '1' && password === '1') {
-//       console.log('Login successful'); // For demonstration purposes
-//       navigation.navigate('Home');
-//     } else {
-//       console.log('Invalid credentials'); // For demonstration purposes
-//       // Handle invalid credentials (e.g., display an error message)
-//     }
-//   };
-  
- 
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={{ position: 'absolute', top: 10, left: 10, marginTop:50}}>
-//           <Ionicons name='arrow-back-outline' size={30}  onPress={LandingPage}/>
-//       </View>
-//       <Text style={styles.head}>HC TECHI</Text>
-
-      
-//       <TextInput
-//         style={styles.input}
-//         placeholder="Username"
-//         placeholderTextColor={'#fff'}
-//         onChangeText={text => setUsername(text)}
-//         value={username}
-//       />
-//       <TextInput
-//         style={styles.input}
-//         styles={{textDecorationLine:'underline'}}
-//         placeholder="Password" 
-//         placeholderTextColor={'#fff'}
-//         onChangeText={text => setPassword(text)}
-//         value={password}
-//         secureTextEntry
-//       />
-//       <TouchableOpacity style={styles.button}   onPress={HomeScreen}>
-//         <Text style={styles.buttonText}>LOGIN</Text>
-//       </TouchableOpacity>
-//       <Text style={styles.pass}>Forgot password !!!</Text>
-
-//       <View style={{ flexDirection: 'column',justifyContent:'center',width:165}} >
-//       <FontAwsome.Button name='apple' backgroundColor='#47624f' color='#fff' borderRadius={25} >
-//       <Text style={{color:'white'}} >Sign in with Apple</Text>
-//         </FontAwsome.Button>
-//         </View>
-//         <View style={{marginTop:20,  }} onPress={HomeScreen}>
-//         <FontAwsome.Button name='google' backgroundColor='#47624f' color='white' borderRadius={25} >
-//       <Text  style={{color:'white',}}>Sign in with Google</Text>
-//         </FontAwsome.Button>
-//         </View>
-      
-      
-      
-        
-      
-//    </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#52AD9C',
-  
-//   },
-//   input: {
-//     width: 300,
-//     height: 40,
-//     borderColor: 'gray',
-    
-//     borderWidth: 1,
-//     marginBottom: 20,
-//     paddingHorizontal: 10,
-//   },
-//   button: {
-//     backgroundColor: '#47624f',
-//     paddingVertical: 12,
-//     paddingHorizontal: 32,
-//     borderRadius: 25,
-//     marginBottom:20
-//   },
-//   buttonText: {
-//     color: '#fff',
-//     fontSize: 16,
-//     textAlign: 'center',
-//   },
-//   pass:{
-//     color:'white',
-//     fontWeight: 'normal',
-//     fontSize: 15,
-//     marginBottom:20,
-//     textDecorationLine:'underline'
-//   },
-//   head:{
-//     color:'white',
-//     fontWeight: 'bold',
-//     fontSize: 28,
-//     marginBottom:20
-//   }
-  
-
- 
-  
-// });
 
 //android : 101582001873-3cdlfcp15b2nc8j7ggn57qgahvo5f4b7.apps.googleusercontent.com
 
@@ -171,6 +45,56 @@
         
 //     };
 //   };
+// const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+//   clientId: "101582001873-3cdlfcp15b2nc8j7ggn57qgahvo5f4b7.apps.googleusercontent.com",
+// });
+
+// const handleSignInWithGoogle = async () => {
+//   try {
+//     const { type, user } = await Google.logInAsync();
+//     if (type === 'success') {
+//       await AsyncStorage.setItem('@user', user.idToken);
+//       navigation.navigate('Home');
+//     } else {
+//       console.log('Google login failed');
+//     }
+//   } catch (error) {
+//     console.error('Error signing in with Google:', error);
+//   }
+// };
+// useEffect(() => {
+//   if (response?.type === 'success') {
+//     const { id_token } = response.params;
+//     AsyncStorage.setItem('@user', id_token);
+//     navigation.navigate('Home');
+//   }
+// }, [response]);
+
+// const getUserInfo = async (token) => {
+//   try {
+//     const userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+//       headers: { Authorization: `Bearer ${token}` },
+//     });
+//     const user = await userInfoResponse.json();
+//     AsyncStorage.setItem('@user', JSON.stringify(user));
+//     setUserInfo(user);
+//     navigation.navigate('Home');
+//   } catch (error) {
+//     console.error('Error fetching user info:', error);
+//   }
+// };
+
+
+// const handleSignOut = async () => {
+//   try {
+//     await AsyncStorage.removeItem('@user');
+//     setUserInfo(null);
+//   } catch (error) {
+//     console.error('Error signing out:', error);
+//   }
+// };
+
+
 
 import React, { useState, } from 'react';
 import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
@@ -180,10 +104,11 @@ import axios from 'axios';
 import Evilicons from '@expo/vector-icons/EvilIcons';
 import FontAwsome from '@expo/vector-icons/FontAwesome';
 import * as WebBrowser from "expo-web-browser";
-import* as Google from "expo-auth-session/providers/google";
+import * as Google from 'expo-auth-session/providers/google';
+import { GoogleSignin,statusCodes } from 'react-native-google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useEffect } from 'react';
-
+import {GoogleAuthProvider} from '@firebase/auth'
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -192,68 +117,50 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const[userinfo,setUserInfo]= useState(null);
   const navigation = useNavigation();
-
+  // const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+  //   clientId: '101582001873-ck1ksio14k72927ktfu82sgc0f1aii2j.apps.googleusercontent.com',
+  // });
   // const handleSignInWithGoogle = async () => {
   //   try {
-  //     const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-  //       clientId: "101582001873-3cdlfcp15b2nc8j7ggn57qgahvo5f4b7.apps.googleusercontent.com",
-  //     });
-
-  //     await promptAsync();
-
-  //     if (response?.type === 'success') {
-  //       const { id_token } = response.params;
-  //       await AsyncStorage.setItem('@user', id_token);
-  //       navigation.navigate('Home');
-  //     } else {
-  //       // Handle sign-in failure
-  //     }
+  //     promptAsync();
   //   } catch (error) {
   //     console.error('Error signing in with Google:', error);
   //   }
   // };
 
-  useEffect(() => {
-    if (response?.type === 'success') {
-      getUserInfo(response.authentication.accessToken);
-    }
-  }, [response]);
-
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: "101582001873-3cdlfcp15b2nc8j7ggn57qgahvo5f4b7.apps.googleusercontent.com",
+  // const handleSignOut = async () => {
+  //   try {
+  //     await AsyncStorage.removeItem('@user');
+  //     // Add any additional sign-out logic here
+  //   } catch (error) {
+  //     console.error('Error signing out:', error);
+  //   }
+  // };
+  GoogleSignin.configure({
+    
+    ClientId: '101582001873-ck1ksio14k72927ktfu82sgc0f1aii2j.apps.googleusercontent.com' 
   });
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      AsyncStorage.setItem('@user', id_token);
-      navigation.navigate('Home');
-    }
-  }, [response]);
-
-  const getUserInfo = async (token) => {
+   const signIn = async () => {
     try {
-      const userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      const user = await userInfoResponse.json();
-      AsyncStorage.setItem('@user', JSON.stringify(user));
-      setUserInfo(user);
-      navigation.navigate('Home');
+      await GoogleSignin.hasPlayServices();
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredentials=GoogleAuthProvider.credential(idToken)
+      await signInwithcredentials( auth, googleCredentials);
+
     } catch (error) {
-      console.error('Error fetching user info:', error);
+      console.log('got error:', error.message)
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
     }
   };
-
-
-  const handleSignOut = async () => {
-    try {
-      await AsyncStorage.removeItem('@user');
-      setUserInfo(null);
-    } catch (error) {
-      console.error('Error signing out:', error);
-    }
-  };
+  
 
   const SERVER_IP = '172.20.10.7';
 
@@ -354,10 +261,10 @@ const Login = () => {
         </FontAwsome.Button>
       </View>
       <View style={{ marginTop: 20 }}>
-        <FontAwsome.Button name='google' backgroundColor='#47624f' color='white' borderRadius={25} onPress={() => promptAsync()} >
+        <FontAwsome.Button name='google' backgroundColor='#47624f' color='white' borderRadius={25} onPress={()=>signIn()} >
           <Text style={{ color: 'white' }}>Sign in with Google</Text>
         </FontAwsome.Button>
-        {userinfo && <Button title='Sign out' onPress={handleSignOut} />}
+        {/* {userinfo && <Button title='Sign out' onPress={signOut} />} */}
       </View>
     </View>
   );
